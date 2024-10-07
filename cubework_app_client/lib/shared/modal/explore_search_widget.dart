bfee60b016578cdea6b05346fb1bd986915005db
darylekarldevera <daryle.devera@cubework.com.ph>
2024-10-08 04:20:41 +0800

    Refactor ExploreSearchWidget to improve date selection and location functionality

--- cubework_app_client/lib/shared/modal/explore_search_widget.dart
+++ cubework_app_client/lib/shared/modal/explore_search_widget.dart
@@ -1,10 +1,56 @@
-import 'package:cubework_app_client/shared/modal/date_picker_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:cubework_app_client/shared/modal/search_modal.dart';
+import 'package:cubework_app_client/utils/format_date.dart';
 
-class ExploreSearchWidget extends StatelessWidget {
-  final void Function(String) searchBarCloseViewCallback;
-  ExploreSearchWidget({super.key, required this.searchBarCloseViewCallback});
+import 'package:cubework_app_client/shared/modal/search_modal.dart';
+import 'package:cubework_app_client/shared/modal/date_picker_widget.dart';
+
+class ExploreSearchWidget extends StatefulWidget {
+  final void Function() searchBarCloseViewCallback;
+  const ExploreSearchWidget({
+    super.key,
+    required this.searchBarCloseViewCallback,
+  });
+
+  @override
+  State<ExploreSearchWidget> createState() => _ExploreSearchWidgetState();
+}
+
+class _ExploreSearchWidgetState extends State<ExploreSearchWidget> {
+  String? _startTime;
+  String? _endTime;
+  DateTime? _startDate;
+  DateTime? _endDate;
+  String? _location;
+
+
+  void Function(DateTime date, String time) get _startDateCallback => (DateTime date, String time) {
+        setState(() {
+          _startDate = date;
+          _startTime = time;
+        });
+      };
+
+  void Function(DateTime date, String time) get _endDateCallBack =>
+      (DateTime date, String time) {
+        setState(() {
+          _endDate = date;
+          _endTime = time;
+        });
+      };
+
+  void Function(String) get getLocation => (String value) {
+        setState(() {
+          _location = value;
+        });
+      };
+
+  @override
+  void initState() {
+    super.initState();
+    _startDate = null;
+    _endDate = null;
+    _location = null;
+  }
 
   final List<Map<String, dynamic>> searchBtn = [
     {
@@ -25,20 +71,65 @@ class ExploreSearchWidget extends StatelessWidget {
         if (value == "Location") {
           SearchModal.show(
             context,
-            searchBarCloseViewCallback,
+            getLocation,
           );
           return;
         }
 
         if (value == "Start Date") {
-          DatePickerWidget.show(context);
+          DatePickerWidget.show(context, _startDateCallback);
           return;
         }
 
         if (value == "End Date") {
-          // Add your code here
+          DatePickerWidget.show(context, _endDateCallBack);
           return;
         }
+      };
+
+  
+  Function(String) get buttonTitleHandler => (String value) {
+        if (value == "Start Date" && _startDate != null) {
+          return '${formatDate(_startDate!)}, $_startTime';
+        }
+
+        if (value == "End Date" && _endDate == null && _startDate != null) {
+          return '${formatDate(_startDate!)}, $_startTime';
+        }
+
+        if (value == "End Date" && _endDate != null) {
+          return '${formatDate(_endDate!)}, $_endTime';
+        }
+
+        if (value == "Location" && _location != null) {
+          return _location!;
+        }
+
+        return value;
+      };
+
+  Function(String) get buttonColorHandler => (String value) {
+      if (value == "Start Date" && _location == null) {
+        return Colors.grey.shade500;
+      }   
+
+      if (value == "End Date" && _startDate == null) {
+        return Colors.grey.shade500;
+      }
+
+        return Colors.black;
+      };
+
+  Function(String) get disableButtonHandler => (String value) {
+        if (value == "Start Date" && _location == null) {
+          return true;
+        }
+
+        if (value == "End Date" && _startDate == null) {
+          return true;
+        }
+
+        return false;
       };
 
   @override
@@ -50,8 +141,8 @@ class ExploreSearchWidget extends StatelessWidget {
     final mediaQueryHeight = mediaQuery.size.height * sixPercentHeight;
 
     return SafeArea(
-      top: false, // Disable top SafeArea padding if not needed
-      bottom: false, // Disable bottom SafeArea padding
+      top: false, 
+      bottom: false,
       child: Container(
         alignment: Alignment.topCenter,
         width: mediaQuery.size.width,
@@ -81,11 +172,10 @@ class ExploreSearchWidget extends StatelessWidget {
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10),
                   child: ListTile(
-                    selectedColor: Colors.grey[300],
-                    hoverColor: Colors.grey[300],
                     leading: Icon(btn["icon"]),
-                    title: Text(btn['textField']),
+                    title: Text(buttonTitleHandler(btn['textField']), style: TextStyle(color: buttonColorHandler(btn['textField']))),
                     onTap: () {
+                      if (disableButtonHandler(btn['textField'])) return;
                       _onSearch(context, btn['textField']);
                     },
                   ),
