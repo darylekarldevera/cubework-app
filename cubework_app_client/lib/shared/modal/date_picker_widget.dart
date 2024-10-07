4876a37b59fa9cbf499cec6a9c7dbb938ce3a7e1
darylekarldevera <daryle.devera@cubework.com.ph>
2024-10-08 04:20:18 +0800

    Refactor DatePickerModal: adjust available times and meridiem options based on date, add select button functionality, fix empty 'availableTimes' error

--- cubework_app_client/lib/shared/modal/date_picker_widget.dart
+++ cubework_app_client/lib/shared/modal/date_picker_widget.dart
@@ -1,52 +1,75 @@
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:cubework_app_client/utils/format_date.dart';
+
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class DatePickerWidget extends StatefulWidget {
-  const DatePickerWidget({super.key});
+  final void Function(DateTime, String) datePickerHandler;
+  const DatePickerWidget({super.key, required this.datePickerHandler});
 
   @override
   State<DatePickerWidget> createState() => _DatePickerWidgetState();
 
   // Static method to show the modal
-  static void show(BuildContext context) {
+  static void show(
+      BuildContext context, void Function(DateTime, String) datePickerHandler) {
     showModalBottomSheet(
       context: context,
-      isScrollControlled: true, 
+      isScrollControlled: true,
       builder: (context) {
-        return const DatePickerWidget(); // Show the stateful widget here
+        return DatePickerWidget(
+          datePickerHandler: datePickerHandler,
+        ); // Show the stateful widget here
       },
     );
   }
 }
 
 class _DatePickerWidgetState extends State<DatePickerWidget> {
-  final now = DateTime.now();
   late DateTime selectedDate;
   late List<String> availableTimes;
   late String selectedTime;
+  late String appBarTitle = "Choose Pickup Time";
+
+  late String todayMeridiemIndicator = DateFormat('a').format(DateTime.now());
   late String selectedMeridiemIndicator;
+  final FocusNode dropDownFocus = FocusNode();
 
   @override
   void initState() {
     super.initState();
     selectedDate = DateTime.now();
-    generateAvailableTimes();
-    selectedTime = availableTimes.first;
-    selectedMeridiemIndicator = DateFormat('a').format(now);
+    generateAvailableTimes(selectedDate);
+
+    selectedTime = availableTimes.isNotEmpty
+        ? availableTimes.first
+        : DateFormat.Hm().format(DateTime.now());
+
+    selectedMeridiemIndicator = DateFormat('a').format(selectedDate);
+
+    dropDownFocus.addListener(() {
+      setState(() {
+        appBarTitle = dropDownFocus.hasFocus
+            ? "Choose Reserve Time"
+            : "Choose Pickup Time";
+      });
+    });
   }
 
-  void generateAvailableTimes() {
+  void generateAvailableTimes(DateTime newDate) {
     DateTime currentTime;
-    final endOfDay = DateTime(
-        now.year, now.month, now.day, 23, 59);
+    final endOfDay = DateTime(newDate.year, newDate.month, newDate.day, 23, 59);
 
     // If the time is before 30 minutes in the hour, round to the next half hour
     // If the time is after 30 minutes, round up to 1:30 of the next hour
-    if (now.minute < 30) {
-      currentTime = DateTime(now.year, now.month, now.day, now.hour, 30);
+    if (newDate.minute < 30) {
+      currentTime =
+          DateTime(newDate.year, newDate.month, newDate.day, newDate.hour, 30);
     } else {
       // Move to the next hour and start at 30 minutes past that hour
-      currentTime = DateTime(now.year, now.month, now.day, now.hour + 1, 30);
+      currentTime = DateTime(
+          newDate.year, newDate.month, newDate.day, newDate.hour + 1, 30);
     }
 
     List<String> times = [];
@@ -64,10 +87,38 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
     });
   }
 
+  void onDateChangedHandler(DateTime date) {
+    final formattedDate = formatDate(date);
+    final formattedTodaysDate = formatDate(DateTime.now());
+    final isTodaySelectedDate = formattedDate == formattedTodaysDate;
+    final newDate = isTodaySelectedDate ? DateTime.now() : date;
+    setState(() {
+      selectedDate = newDate;
+      generateAvailableTimes(newDate);
+      todayMeridiemIndicator =
+          isTodaySelectedDate ? DateFormat('a').format(DateTime.now()) : "AM";
+    });
+  }
+
+  Color isMeridiemIndicatorAvailable() {
+    if (todayMeridiemIndicator == "PM") {
+      return Colors.grey;
+    }
+    return Colors.black;
+  }
+
+  bool isSelectButtonEnabled() {
+    if (availableTimes.isNotEmpty &&
+        availableTimes.first == selectedTime) {
+      return false;
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.65, 
+      height: MediaQuery.of(context).size.height * 0.75,
       child: Scaffold(
           backgroundColor: const Color.fromARGB(0, 255, 255, 255),
           appBar: AppBar(
@@ -78,16 +129,16 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
               ),
             ),
             title: Container(
-              // padding: const EdgeInsets.only(top: 16),
+              margin: const EdgeInsets.only(top: 6),
               alignment: Alignment.topLeft,
-              child: const Text("Choose Pickup Time",
-                  style: TextStyle(fontWeight: FontWeight.bold)),
+              child: Text(appBarTitle,
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
             ),
             automaticallyImplyLeading:
                 false, // Prevents the default leading (back arrow) from appearing
             actions: [
               Padding(
-                padding: const EdgeInsets.all(0),
+                padding: const EdgeInsets.only(right: 8.0),
                 child: CircleAvatar(
                   backgroundColor: Colors
                       .grey.shade300, // Background color inside the circle
@@ -127,16 +178,13 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
                     initialDate: selectedDate,
                     firstDate: DateTime.now(),
                     lastDate: DateTime.now().add(const Duration(days: 365)),
-                    onDateChanged: (DateTime date) {
-                      setState(() {
-                        selectedDate = date;
-                      });
-                    },
+                    onDateChanged: (DateTime date) => onDateChangedHandler(date),
                   ),
                 ),
               ),
               Flexible(
                 child: Container(
+                  height: MediaQuery.of(context).size.height * 0.07,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: Colors.white,
@@ -156,40 +204,62 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
                                 const Text("Time",
                                     style: TextStyle(fontSize: 16)),
                                 Container(
-                                  margin: const EdgeInsets.all(10),
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 16, vertical: 6),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.grey.shade100,
-                                  ),
-                                  child: DropdownButton<String>(
-                                    isDense: true,
-                                    style: const TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                    icon:
-                                        Container(), // Hides the dropdown icon
-                                    underline:
-                                        Container(), // Removes the underline
-                                    menuMaxHeight: 200,
-                                    value: selectedTime,
-                                    onChanged: (String? newValue) {
-                                      setState(() {
-                                        selectedTime = newValue!;
-                                      });
-                                    },
-                                    items: availableTimes
-                                        .map<DropdownMenuItem<String>>(
-                                            (String time) {
-                                      return DropdownMenuItem<String>(
-                                        value: time,
-                                        child: Center(child: Text(time)),
-                                      );
-                                    }).toList(),
-                                  ),
-                                ),
+                                    margin: const EdgeInsets.all(10),
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 16, vertical: 6),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.grey.shade100,
+                                    ),
+                                    child: availableTimes.isEmpty
+                                        ? const Text(
+                                            "No available times",
+                                            style: TextStyle(fontSize: 16),
+                                          )
+                                        : DropdownButton2<String>(
+                                            focusNode: dropDownFocus,
+                                            isDense: true,
+                                            iconStyleData: IconStyleData(
+                                              icon: Container(),
+                                            ),
+                                            style: const TextStyle(
+                                                color: Colors.black,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16),
+                                            dropdownStyleData:
+                                                DropdownStyleData(
+                                                    offset: Offset(
+                                                        0,
+                                                        MediaQuery.of(context)
+                                                                .size
+                                                                .height *
+                                                            -0.025),
+                                                    maxHeight:
+                                                        MediaQuery.of(context)
+                                                                .size
+                                                                .height *
+                                                            0.18,
+                                                    width: double.infinity),
+                                            value: selectedTime,
+                                            onChanged: (String? newValue) {
+                                              setState(() {
+                                                if (newValue != null) {
+                                                  selectedTime = newValue;
+                                                }
+                                              });
+                                            },
+                                            items: availableTimes.isNotEmpty
+                                                ? availableTimes.map<
+                                                    DropdownMenuItem<
+                                                        String>>((String time) {
+                                                    return DropdownMenuItem<
+                                                        String>(
+                                                      value: time,
+                                                      child: Text(time),
+                                                    );
+                                                  }).toList()
+                                                : [],
+                                          )),
                               ],
                             ),
                           ),
@@ -203,6 +273,7 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
                               GestureDetector(
                                 onTap: () {
                                   setState(() {
+                                    if (todayMeridiemIndicator == "PM") return;
                                     selectedMeridiemIndicator = "AM";
                                   });
                                 },
@@ -212,10 +283,10 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
                                   color: selectedMeridiemIndicator != "AM"
                                       ? null
                                       : Colors.grey.shade100,
-                                  child: const Text("AM",
+                                  child: Text("AM",
                                       style: TextStyle(
                                           fontSize: 16,
-                                          fontWeight: FontWeight.bold)),
+                                          fontWeight: FontWeight.bold, color: isMeridiemIndicatorAvailable())),
                                 ),
                               ),
                               GestureDetector(
@@ -244,14 +315,19 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
                   ),
                 ),
               ),
-              // change it into sizebox instead of container
+              const Spacer(),
               Container(
-                margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                child: ElevatedButton(
-                      onPressed: () => {},
+                  margin: const EdgeInsets.only(left: 15, right: 15),
+                  child: ElevatedButton(
+                      onPressed: () => {
+                            widget.datePickerHandler(selectedDate, selectedTime),
+                            Navigator.of(context).pop()
+                          },
                       style: ButtonStyle(
                         backgroundColor: WidgetStateProperty.all(
-                          Colors.grey.shade100,
+                          isSelectButtonEnabled()
+                              ? Colors.green
+                              : Colors.grey.shade100,
                         ),
                         shape: WidgetStateProperty.all(
                           RoundedRectangleBorder(
@@ -263,11 +339,14 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
                         padding: const EdgeInsets.symmetric(vertical: 20),
                         width: double.infinity,
                         alignment: Alignment.center,
-                        child: const Text("Select",
+                        child: Text("Select",
                             style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
-                      ))
-              )
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: isSelectButtonEnabled()
+                                    ? Colors.white
+                                    : Colors.black)),
+                      )))
             ],
           )),
     );
