e027511b1a647a481d74e5212928ab1350a2236c
darylekarldevera <daryle.devera@cubework.com.ph>
2024-10-08 08:07:11 +0800

    Refactor MainApp to update the number of tabs to match the number of screens

--- cubework_app_client/lib/main.dart
+++ cubework_app_client/lib/main.dart
@@ -10,34 +10,34 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   TabBar get _tabBar => const TabBar(
-    labelColor: Color(0xFF229E1B),
-    tabs: [
-      Tab(icon: Icon(Icons.home), text: "Home"),
-      Tab(icon: Icon(Icons.person), text: "My Account"),
-      Tab(icon: Icon(Icons.list), text: "Orders"),
-    ],
-  );
+        labelColor: Color(0xFF229E1B),
+        tabs: [
+          Tab(icon: Icon(Icons.home), text: "Home"),
+          Tab(icon: Icon(Icons.person), text: "My Account"),
+          Tab(icon: Icon(Icons.list), text: "Orders"),
+        ],
+      );
 
   TabBarView get _tabBarView => const TabBarView(
-    children: [
-      ExploreScreen(),
-      MyAccountScreen(),
-      Center(child: Text("Orders")),
-    ],
-  );
+        children: [
+          ExploreScreen(),
+          MyAccountScreen(),
+          Center(child: Text("Orders")),
+        ],
+      );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: DefaultTabController(
-        length: 5, 
+        length: 3, // Change this to 3 to match the number of tabs
         child: Scaffold(
           appBar: AppBar(),
           bottomNavigationBar: _tabBar,
           body: _tabBarView,
-        )
-      )
+        ),
+      ),
     );
   }
 }
