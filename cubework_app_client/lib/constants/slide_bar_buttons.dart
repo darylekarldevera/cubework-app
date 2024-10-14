14f3a40106d9869607736b9f4aac2622755dfd3c
darylekarldevera <daryle.devera@cubework.com.ph>
2024-10-14 08:38:26 +0800

    Add slide bar buttons constants for live, wow, office, industrial, and flex options

---
+++ cubework_app_client/lib/constants/slide_bar_buttons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final List<Map<String, dynamic>> slideBarButtons = [
+  {'textField': "Live", 'icon': Icons.cell_tower},
+  {'textField': "Wow", 'icon': Icons.emoji_emotions_outlined},
+  {'textField': "Office", 'icon': Icons.business},
+  {'textField': "Industrial", 'icon': Icons.factory_outlined},
+  {'textField': "Flex", 'icon': Icons.warehouse_outlined},
+];
