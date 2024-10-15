03e2a0a4153d2aae466845191368caddb37dbab3
darylekarldevera <daryle.devera@cubework.com.ph>
2024-10-15 08:57:53 +0800

    Add Office class to represent an office with address, coordinates, name, and city

---
+++ cubework_app_client/lib/interfaces/office.dart
@@ -0,0 +1,15 @@
+class Office {
+  final String address;
+  final double lat;
+  final double lng;
+  final String name;
+  final String city;
+  
+  Office({
+    required this.address,
+    required this.lat,
+    required this.lng,
+    required this.name,
+    required this.city,
+  });
+}
