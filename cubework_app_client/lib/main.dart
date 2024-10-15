ab6707c0cd6d47bf521a52acba2e79a91774c0a2
darylekarldevera <daryle.devera@cubework.com.ph>
2024-10-15 08:57:32 +0800

    Add location permissions to AndroidManifest.xml and set up permission handling in main.dart

--- cubework_app_client/lib/main.dart
+++ cubework_app_client/lib/main.dart
@@ -1,9 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:cubework_app_client/screens/explore_screen.dart';
 import 'package:cubework_app_client/screens/my_account_screen.dart';
-import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(const MainApp());
+// this is to allow the google map 'myLocationButtonEnabled' and 'myLocationEnabled' to work
+void setPermissions() async {
+  PermissionStatus status = await Permission.location.request();
+  if (status.isGranted) {
+    print("Location permission granted");
+  } else if (status.isDenied) {
+    print("Location permission denied");
+  } else if (status.isPermanentlyDenied) {
+    print("Location permission permanently denied");
+    openAppSettings();
+  }
+}
+
+void main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    setPermissions();
+    runApp(const MainApp());
+  } catch (error) {
+    print("Error: $error");
+  }
 }
 
 class MainApp extends StatelessWidget {
