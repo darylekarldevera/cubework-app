b5d26f2f788dc41955b33b26f70be6471cae48dd
darylekarldevera <daryle.devera@cubework.com.ph>
2024-10-17 04:19:14 +0800

    Add camelToSentence utility function

---
+++ lib/utils/camel_to_sentence.dart
@@ -0,0 +1,5 @@
+String camelToSentence(String text) {
+  var result = text.replaceAll(RegExp(r'(?<!^)(?=[A-Z])'), r" ");
+  var finalResult = result[0].toUpperCase() + result.substring(1);
+  return finalResult;
+}
