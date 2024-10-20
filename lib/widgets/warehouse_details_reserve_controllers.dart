ba38532dc0e94fe8a2934c30b06ce8cc864f95a0
darylekarldevera <daryle.devera@cubework.com.ph>
2024-10-21 05:20:26 +0800

    Refactor WarehouseDetailsReserveControllers to add date selection functionality

--- lib/widgets/warehouse_details_reserve_controllers.dart
+++ lib/widgets/warehouse_details_reserve_controllers.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
+
+import 'package:cubework_app_client/utils/format_date.dart';
+import 'package:cubework_app_client/shared/modal/date_confirmation.dart';
+import 'package:cubework_app_client/interfaces/reserved_warehouse.dart';
+import 'package:cubework_app_client/shared/modal/date_picker_widget.dart';
+import 'package:cubework_app_client/shared/modal/warehouse_reserve_modal.dart';
 import 'package:cubework_app_client/widgets/warehouse_details_reserve_button.dart';
 
-class WarehouseDetailsReserveControllers extends StatelessWidget {
+class WarehouseDetailsReserveControllers extends StatefulWidget {
   const WarehouseDetailsReserveControllers({super.key});
+
+  @override
+  State<WarehouseDetailsReserveControllers> createState() =>
+      _WarehouseDetailsReserveControllersState();
+}
+
+class _WarehouseDetailsReserveControllersState
+    extends State<WarehouseDetailsReserveControllers> {
+  DateTimeInfo? startDate;
+  DateTimeInfo? endDate;
+
+  void Function(DateTime date, String formattedDate, String formattedTime)
+      get startDateCallback =>
+          (DateTime date, String formattedDate, String formattedTime) {
+            setState(() {
+              startDate = DateTimeInfo(date: date, time: formattedTime);
+            });
+          };
+
+  void Function(DateTime date, String formattedDate, String formattedTime)
+      get endDateCallback =>
+          (DateTime date, String formattedDate, String formattedTime) {
+            setState(() {
+              endDate = DateTimeInfo(date: date, time: formattedTime);
+            });
+          };
+
+  String getDateString(DateTimeInfo? selectedDate, String title) {
+    if (selectedDate == null) {
+      return title;
+    }
+
+    final String formattedDate = formatDate(selectedDate.date!);
+    return formattedDate;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +52,25 @@ class WarehouseDetailsReserveControllers extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            WarehouseDetailsReserveButton(onTap: () => {}, title: "Start Date"),
+            WarehouseDetailsReserveButton(
+              onTap: () => {DatePickerWidget.show(context, startDateCallback)},
+              title: getDateString(startDate, "Start Date"),
+            ),
             const SizedBox(width: 40),
-            WarehouseDetailsReserveButton(onTap: () => {}, title: "End Date"),
+            WarehouseDetailsReserveButton(
+              onTap: () => {DatePickerWidget.show(context, endDateCallback)},
+              title: getDateString(endDate, "End Date"),
+            ),
           ],
         ),
         GestureDetector(
-          onTap: () => {},
+          onTap: () => {
+            if (startDate != null && endDate != null) {
+              DateConfirmation.show(context, startDate!, endDate!)
+            } else {
+              WarehouseReserveModal.show(context)
+            }
+          },
           child: Container(
             padding: const EdgeInsets.all(8),
             margin: const EdgeInsets.symmetric(vertical: 15),
