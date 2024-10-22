6f4b95cb1efc8e3f2d8e3fca1d2c2ad3f896f13c
darylekarldevera <daryle.devera@cubework.com.ph>
2024-10-22 13:30:19 +0800

    Add constants for space types

---
+++ lib/constants/space_types.dart
@@ -0,0 +1,21 @@
+Map<String, String> spaceTypes = {
+  'warehouseSpace': 'Warehouse Space',
+  'privateOffice': 'Private Office',
+  'eventSpace': 'Event Space',
+  'broadcastStudios': 'Broadcast Studios',
+  'pickleBall': 'Pickle Ball',
+  'crossDocks': 'Cross Docks',
+  'meetingRoom': 'Meeting Room',
+  'coworkingSpace': 'Coworking Space',
+  'conferenceHall': 'Conference Hall',
+  'storageUnits': 'Storage Units',
+  'virtualOffice': 'Virtual Office',
+  'industrialSpace': 'Industrial Space',
+  'retailSpace': 'Retail Space',
+  'sharedOffice': 'Shared Office',
+  'fitnessCenter': 'Fitness Center',
+  'laboratorySpace': 'Laboratory Space',
+  'creativeStudio': 'Creative Studio',
+  'trainingRoom': 'Training Room',
+  'researchFacility': 'Research Facility'
+};
